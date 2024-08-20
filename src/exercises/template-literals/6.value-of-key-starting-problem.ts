3723e7ed89fa59682f95286c1dc80df085efe6d3
VinhNguyen <vinh@cck.co.jp>
2024-08-20 22:02:25 +0700

    [Update]

--- src/exercises/template-literals/6.value-of-key-starting-problem.ts
+++ src/exercises/template-literals/6.value-of-key-starting-problem.ts
@@ -1,13 +1,17 @@
 export type Obj = {
-  a: "a";
-  a2: "a2";
-  a3: "a3";
-  b: "b";
-  b1: "b1";
-  b2: "b2";
+  a: 'a';
+  a2: 'a2';
+  a3: 'a3';
+  b: 'b';
+  b1: 'b1';
+  b2: 'b2';
 };
 
-type ValuesOfKeysStartingWithA<Obj> = unknown;
+type StartWithA<T> = T extends `a${infer R}` ? `a${R}` : never;
+
+type ValuesOfKeysStartingWithA<Obj> = {
+  [K in keyof Obj as `${Extract<keyof Obj, StartWithA<keyof Obj>>}`]: string;
+};
 
 type NewUnion = ValuesOfKeysStartingWithA<Obj>;
 // type NewUnion = "a" | "a2" | "a3";
